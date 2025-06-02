79c80b2a0f4a4c79895fd881404ab737a394743b
Nicolas Oliver <dario.n.oliver@intel.com>
2025-06-02 10:05:19 -0700

    Refactor Test
    * Add config with dotenv
    * Add logging

--- metro-ai-suite/image-based-video-search/test/test.py
+++ metro-ai-suite/image-based-video-search/test/test.py
@@ -1,21 +1,44 @@
+import logging
+import os
 import time
 import unittest
 
+from dotenv import dotenv_values, load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+logging.basicConfig(level=logging.INFO)
+
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+ENV_FILE_PATH = os.path.join(FILE_PATH, ".env")
+
+load_dotenv(ENV_FILE_PATH)
 
 
 class ImageBasedVideoSearchTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
-        cls.driver.get("http://localhost:3000")
+        cls.config = dotenv_values(ENV_FILE_PATH)
+        options = webdriver.ChromeOptions()
+        options.add_argument("window-size=1280,800")
+        cls.driver = webdriver.Chrome(options=options)
+        cls.driver.get(cls.config["ROOT_URL"])
+        cls.logger = logging.getLogger("ImageBasedVideoSearchTest")
         time.sleep(2)
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
+        pass
+
+    def find_button(self, button_text):
+        """
+        Helper method to find a button by its text.
+        """
+        return self.driver.find_element(
+            By.XPATH, f"//button[contains(text(), '{button_text}')]"
+        )
 
     def test_startup(self):
         # Assert page title is correct
@@ -42,66 +65,46 @@ class ImageBasedVideoSearchTest(unittest.TestCase):
             "Clear Database",
         ]
         for text in button_texts:
-            button = self.driver.find_element(
-                By.XPATH, f"//button[contains(text(), '{text}')]"
-            )
+            button = self.find_button(text)
             self.assertIsNotNone(button)
 
     def test_live_search(self):
 
-        # Select "Analyze Stream" button
-        analyze_button = self.driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Analyze Stream')]"
-        )
-
-        # Start Analysis
+        # Start Video Analysis
+        self.logger.info("Starting Video Stream Analysis...")
+        analyze_button = self.find_button("Analyze Stream")
         analyze_button.click()
 
-        # Do analysis for 60 seconds
-        time.sleep(60)
+        # Wait until analysis is done
+        time.sleep(45)
 
-        # Select "Stop Analysis" button
-        stop_button = self.driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Stop Analysis')]"
-        )
-
-        # Stop Analysis
+        # Stop Video Analysis
+        self.logger.info("Stopping Video Stream Analysis...")
+        stop_button = self.find_button("Stop Analysis")
         stop_button.click()
 
-        # Select "Capture Frame" button
-        capture_button = self.driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Capture Frame')]"
-        )
-
-        # Capture a Frame
+        # Do Image Search
+        self.logger.info("Starting Image Search...")
+        capture_button = self.find_button("Capture Frame")
         capture_button.click()
-
-        # Wait 2 seconds for capture to complete
         time.sleep(2)
-
-        # Select "Search Object" button
-        search_button = self.driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Search Object')]"
-        )
-
-        # Run a search
+        search_button = self.find_button("Search Object")
         search_button.click()
-
-        # Wait 5 seconds for the search to complete
         time.sleep(5)
 
-        # Assert that there are 10 image results
+        # Check Search Results
+        self.logger.info("Checking Search Results...")
         image_list = self.driver.find_element(By.CLASS_NAME, "image-list")
         image_items = image_list.find_elements(By.TAG_NAME, "li")
+        self.logger.info(f"Found {len(image_items)} image results.")
         self.assertEqual(len(image_items), 10)
 
-        # Select "Clear Database" button
-        clear_button = self.driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Clear Database')]"
-        )
-
-        # Clear the database
+        # Clear the Database
+        self.logger.info("Clearing the database...")
+        clear_button = self.find_button("Clear Database")
         clear_button.click()
+        time.sleep(2)
+        self.logger.info("Database cleared successfully.")
 
 
 if __name__ == "__main__":
