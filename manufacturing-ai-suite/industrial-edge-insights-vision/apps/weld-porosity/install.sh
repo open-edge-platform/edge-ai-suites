644a57c4a3a4e14a6b849f5216df040e4c242dd5
Rajput, Sajeev <sajeev.rajput@intel.com>
2025-06-26 14:26:49 +0530

    corrected weld model path

--- manufacturing-ai-suite/industrial-edge-insights-vision/apps/weld-porosity/install.sh
+++ manufacturing-ai-suite/industrial-edge-insights-vision/apps/weld-porosity/install.sh
@@ -20,7 +20,7 @@ download_artifacts() {
         return 1
     fi
     # Download model artifacts if not already present
-    LOCAL_MODEL_DIR="$SCRIPT_DIR/../../resources/$app_name/models"
+    LOCAL_MODEL_DIR="$SCRIPT_DIR/../../resources/$app_name/models/weld-porosity"
     if [ ! -d $LOCAL_MODEL_DIR ]; then
         # create the models directory if it does not exist
         
