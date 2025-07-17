92b4d2e1b18419052b7abc4ea8fdc113363ce9d6
Grzegorz Wrobel <grzegorz.wrobel@intel.com>
2025-07-17 16:57:11 +0200

    Remove not used import

--- metro-ai-suite/smart-intersection/tests/test_components_access.py
+++ metro-ai-suite/smart-intersection/tests/test_components_access.py
@@ -2,8 +2,6 @@
 # SPDX-License-Identifier: LicenseRef-Intel-Edge-Software
 # This file is licensed under the Limited Edge Software Distribution License Agreement.
 
-
-import os
 import pytest
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
