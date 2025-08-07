4c4709d58d128024e89adac18de2228d18a1bec8
Pooja Kumbharkar <pooja.kumbharkar@intel.com>
2025-08-07 12:31:36 +0530

    windturbine_anomaly_detecion: changes to mqtt publisher
    Changes:
     publisher.py - Added iterrow function for looping
    
    Signed-off-by: Pooja Kumbharkar <pooja.kumbharkar@intel.com>

--- manufacturing-ai-suite/wind-turbine-anomaly-detection/simulator/mqtt-publisher/publisher.py
+++ manufacturing-ai-suite/wind-turbine-anomaly-detection/simulator/mqtt-publisher/publisher.py
@@ -86,15 +86,14 @@ def stream_csv(mqttc, topic, subsample, sampling_rate, filename):
 
         tick = g_tick(float(subsample) / float(sampling_rate))
 
-        for i in range(csv_data.shape[0]):
-            row = csv_data.iloc[i]
+        for _, row in csv_data.iterrows():
 
             if subsample > 1 and (row_served % subsample) != 0:
                 row_served += 1
                 continue
 
             try:
-                msg = jencoder.encode({col: csv_data.iloc[i][col] for col in csv_data.columns})
+                msg = jencoder.encode({col: row[col] for col in csv_data.columns})
                 print("Publishing message", msg)
                 mqttc.publish(topic, msg)
             except (ValueError, IndexError):
