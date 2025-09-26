29f5055cba694ffdda09b1e147ee7aa200b5b26f
Tomasz Dorau <tomasz.dorau@intel.com>
2025-09-26 11:37:49 +0200

    Add scripts for running perf tests with RTSP source

--- metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/perf_test/run_dls_2ppl_rtsp.sh
+++ metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/perf_test/run_dls_2ppl_rtsp.sh
@@ -48,5 +48,8 @@ docker run --rm -v ${VIDEOS_DIR}:/data -v ${MODELS_DIR}:/models \
 intel/dlstreamer:2025.1.2-ubuntu24 \
 gst-launch-1.0 \
 rtspsrc location=${INPUT_RTSP} latency=100 protocols=udp ! decodebin3 ! timecodestamper set=always ! vapostproc ! "video/x-raw(memory:VAMemory)" \
- ! gvadetect batch-size=1 scheduling-policy=latency device=GPU model-instance-id=detect1 inference-interval=1 model=/models/intersection/openvino.xml pre-process-backend=va-surface-sharing ! queue \
- ! gvafpscounter interval=1 starting-frame=1000 print-latency=true ! appsink sync=false > logs/run_dls_1ppl_rtsp.log 2>&1 &
+ ! gvadetect batch-size=2 scheduling-policy=latency device=GPU model-instance-id=detect1 inference-interval=1 model=/models/intersection/openvino.xml pre-process-backend=va-surface-sharing ! queue \
+ ! gvafpscounter interval=1 starting-frame=1000 print-latency=true ! appsink sync=false \
+rtspsrc location=${INPUT_RTSP} latency=100 protocols=udp ! decodebin3 ! timecodestamper set=always ! vapostproc ! "video/x-raw(memory:VAMemory)" \
+ ! gvadetect batch-size=2 scheduling-policy=latency device=GPU model-instance-id=detect1 inference-interval=1 model=/models/intersection/openvino.xml pre-process-backend=va-surface-sharing ! queue \
+ ! gvafpscounter interval=1 starting-frame=1000 print-latency=true ! appsink sync=false > logs/run_dls_2ppl_rtsp.log 2>&1 &
