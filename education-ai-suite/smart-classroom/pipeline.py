4a32f2a90563378816ea9bf376867cfaca23a3cf
T <tonika.reddy.t@intel.com>
2025-11-19 13:29:42 +0530

    updates for real time audio

--- education-ai-suite/smart-classroom/pipeline.py
+++ education-ai-suite/smart-classroom/pipeline.py
@@ -50,6 +50,7 @@ class Pipeline:
                 yield chunk_trancription
         finally:
             monitor.stop_monitoring()
+            time.sleep(3) #time for socwatch to get clean-start
             
     
     def run_summarizer(self):
