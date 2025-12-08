b680c621e7533006c5f7a9f76d5629b70258d3b5
Perycz, Przemyslaw <przemyslaw.perycz@intel.com>
2025-12-08 09:54:57 -0800

    Update of ADBSCAN component. Initial support for both Humble and Jazzy ROS2 distros

--- robotics-ai-suite/components/adbscan/Follow_me_RS_2D/src/adbscan_ros2/launch/play_demo_realsense_launch.py
+++ robotics-ai-suite/components/adbscan/Follow_me_RS_2D/src/adbscan_ros2/launch/play_demo_realsense_launch.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -14,22 +15,15 @@ def generate_launch_description():
     adbscan_param_file = f'{install_dir}/config/adbscan_sub_RS.yaml'
     bag_file = f'/opt/ros/{ros_distro}/share/bagfiles/laser-pointcloud'
 
-    return LaunchDescription([
-        # Launch adbscan node with parameters
-        Node(
-            package='adbscan_ros2',
-            executable='adbscan_sub',
-            parameters=[adbscan_param_file]
-        ),
-        # Play rosbag in loop
-        ExecuteProcess(
-            cmd=['ros2', 'bag', 'play', '--loop', bag_file],
-            output='screen'
-        ),
-        # Start RViz with config
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config_file]
-        ),
-    ])
+    return LaunchDescription(
+        [
+            # Launch adbscan node with parameters
+            Node(
+                package='adbscan_ros2', executable='adbscan_sub', parameters=[adbscan_param_file]
+            ),
+            # Play rosbag in loop
+            ExecuteProcess(cmd=['ros2', 'bag', 'play', '--loop', bag_file], output='screen'),
+            # Start RViz with config
+            Node(package='rviz2', executable='rviz2', arguments=['-d', rviz_config_file]),
+        ]
+    )
