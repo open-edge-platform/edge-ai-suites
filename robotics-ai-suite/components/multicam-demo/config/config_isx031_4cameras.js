eca68392d06997bd845cf55560a324d169d465ad
Mohit Mehrotra <mohit.mehrotra@intel.com>
2026-03-16 15:51:44 -0700

    update to get isx031 working

---
+++ robotics-ai-suite/components/multicam-demo/config/config_isx031_4cameras.js
@@ -0,0 +1,42 @@
+[
+        {
+                "name":         "yolov8n-seg",
+                "model":        "/home/intel/github/edge-ai-suites/robotics-ai-suite/components/multicam-demo/models/yolov8/FP16/yolov8n-seg.xml",
+                "device":       "GPU",
+                "data_type":    "FP16",
+                "source":       "/dev/video-isx031-a-0",
+                "adapter":      "yolov8",
+                "width":        1920,
+                "height":       1536
+        },
+        {
+                "name":         "yolov8n-seg",
+                "model":        "/home/intel/github/edge-ai-suites/robotics-ai-suite/components/multicam-demo/models/yolov8/FP16/yolov8n-seg.xml",
+                "device":       "CPU",
+                "data_type":    "FP16",
+                "source":       "/dev/video-isx031-b-0",
+                "adapter":      "yolov8",
+                "width":        1920,
+                "height":       1536
+        },
+        {
+                "name":         "yolov8n",
+                "model":        "/home/intel/github/edge-ai-suites/robotics-ai-suite/components/multicam-demo/models/yolov8/FP16/yolov8n.xml",
+                "device":       "GPU",
+                "data_type":    "FP16",
+                "source":       "/dev/video-isx031-c-0",
+                "adapter":      "yolov8",
+                "width":        1920,
+                "height":       1536
+        },
+        {
+                "name":         "yolov8n",
+                "model":        "/home/intel/github/edge-ai-suites/robotics-ai-suite/components/multicam-demo/models/yolov8/FP16/yolov8n.xml",
+                "device":       "GPU",
+                "data_type":    "FP16",
+                "source":       "/dev/video-isx031-d-0",
+                "adapter":      "yolov8",
+                "width":        1920,
+                "height":       1536
+        }
+]
