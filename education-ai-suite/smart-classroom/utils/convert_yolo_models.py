a7459ca09b545995a2344006c397827084ca018b
Yun Liu <yun.m.liu@intel.com>
2026-04-22 13:07:56 +0800

    up

--- education-ai-suite/smart-classroom/utils/convert_yolo_models.py
+++ education-ai-suite/smart-classroom/utils/convert_yolo_models.py
@@ -176,6 +176,7 @@ def convert_yolo_to_openvino(model_name: str, output_dir: str):
 
     print(f"Quantized model will be saved to {int8_model_pose_path}")
     ov.save_model(quantized_pose_model, str(int8_model_pose_path))
+    del pose_model
     shutil.rmtree(pose_model_dir)
 
 
