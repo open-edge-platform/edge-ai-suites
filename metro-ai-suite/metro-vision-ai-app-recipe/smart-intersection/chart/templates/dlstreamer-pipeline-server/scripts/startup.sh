8c41498c66eece1c576c8f9a39a497e58ed27240
Kumar, Anand <anand.kumar@intel.com>
2026-06-19 14:07:20 +0530

    remove loading driver for tc gpu
    Signed-off-by: Kumar, Anand <anand.kumar@intel.com>

--- metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/templates/dlstreamer-pipeline-server/scripts/startup.sh
+++ metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/templates/dlstreamer-pipeline-server/scripts/startup.sh
@@ -14,13 +14,7 @@ echo "$SMART_INTERSECTION_BROKER_SERVICE_HOST    $MQTT_HOST" >> /etc/hosts &&
 {{- if or .Values.dlstreamerPipelineServer.gpu.enabled .Values.dlstreamerPipelineServer.npu.enabled }}
 ./run.sh
 {{- else if and .Values.trustedCompute.enabled .Values.trustedCompute.tc_gpu_enabled }}
-until LIBVA_DRIVER_NAME=iHD \
-	LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri \
-	vainfo 2>/dev/null | grep -q "VA-API version"; do
-	echo "Waiting for GPU VA-API..."
-	sleep 1
-done &&
-rm -f /home/intelmicroserviceuser/.cache/gstreamer-1.0/registry.x86_64.bin &&
+until vainfo 2>/dev/null | grep -q "VA-API version"; do echo "Waiting for GPU VA-API..."; sleep 1; done &&
 ./run.sh
 {{- else }}
 runuser -u intelmicroserviceuser ./run.sh
