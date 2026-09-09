40759a2bba3233d5a7c3397dfe07c5ba7c6f84d6
Sachin Sharma <sachin.kumar.sharma@intel.com>
2026-09-09 23:07:07 +0530

    document update and traiing improvments changes

--- health-and-life-sciences-ai-suite/surgical-instrument/backend/bootstrap/train.py
+++ health-and-life-sciences-ai-suite/surgical-instrument/backend/bootstrap/train.py
@@ -80,6 +80,8 @@ def train_model(
         workers=train_cfg["workers"],
         device=dev,
         amp=train_cfg["amp"],
+        cache=train_cfg.get("cache", False),
+        fraction=train_cfg.get("fraction", 1.0),
         optimizer=train_cfg["optimizer"],
         lr0=train_cfg["lr0"],
         lrf=train_cfg["lrf"],
