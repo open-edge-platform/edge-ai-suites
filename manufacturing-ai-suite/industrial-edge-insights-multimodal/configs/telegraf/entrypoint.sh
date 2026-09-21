ffc53d8ad297b4ace1229a69bb315979486a53b9
Tanvi Dhola <tanvi.dhola@intel.com>
2026-09-21 10:26:34 +0530

    Addressed issues from scan
    Signed-off-by: Tanvi Dhola <tanvi.dhola@intel.com>

--- manufacturing-ai-suite/industrial-edge-insights-multimodal/configs/telegraf/entrypoint.sh
+++ manufacturing-ai-suite/industrial-edge-insights-multimodal/configs/telegraf/entrypoint.sh
@@ -4,6 +4,11 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-export DEBUG_MODE=$( [ \"$LOG_LEVEL\" = \"DEBUG\" ] && echo true || echo false )
+if [ "$LOG_LEVEL" = "DEBUG" ]; then
+	DEBUG_MODE=true
+else
+	DEBUG_MODE=false
+fi
+export DEBUG_MODE
 
 telegraf --non-strict-env-handling --config "${TELEGRAF_CONFIG_PATH}" --input-filter "${TELEGRAF_INPUT_PLUGIN}"
